e8d2b42a8bdfff8f66e9110c6d149b5c65d2d4ca
Michael Parmeley <parmeleym@gmail.com>
2023-05-30 07:24:28 -0700

    Fizz Buzz answered

--- lab-answers.js
+++ lab-answers.js
@@ -8,10 +8,24 @@ R-ALAB 308H.4.2
 // Easy Going
 ////////////////////////////
 
-for(let i = 1; i <= 20; i++){
-    console.log(i);
-}
+// for(let i = 1; i <= 20; i++){
+//     console.log(i);
+// }
 
 ////////////////////////////
-// 
+// Fizz Buzz
 ////////////////////////////
+
+for(let i = 1; i <= 100; i++){
+    if(i % 3 == 0 && i % 5 == 0){
+        console.log("FizzBUzz");
+    }
+    else if(i % 3 ==0){
+        console.log("Fizz");
+    }
+    else if(i % 5 == 0){
+        console.log("Buzz");
+    }else{
+        console.log(i);
+    }
+}
