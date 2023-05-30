e8c8c8e369510895a55dba3c514a8386d1e6bc37
Michael Parmeley <parmeleym@gmail.com>
2023-05-30 07:45:03 -0700

    Wild Wild Life answered

--- lab-answers.js
+++ lab-answers.js
@@ -16,16 +16,60 @@ R-ALAB 308H.4.2
 // Fizz Buzz
 ////////////////////////////
 
-for(let i = 1; i <= 100; i++){
-    if(i % 3 == 0 && i % 5 == 0){
-        console.log("FizzBUzz");
-    }
-    else if(i % 3 ==0){
-        console.log("Fizz");
-    }
-    else if(i % 5 == 0){
-        console.log("Buzz");
-    }else{
-        console.log(i);
+// for(let i = 1; i <= 100; i++){
+//     if(i % 3 == 0 && i % 5 == 0){
+//         console.log("FizzBUzz");
+//     }
+//     else if(i % 3 ==0){
+//         console.log("Fizz");
+//     }
+//     else if(i % 5 == 0){
+//         console.log("Buzz");
+//     }else{
+//         console.log(i);
+//     }
+// }
+
+////////////////////////////
+// Wild Wild Life
+////////////////////////////
+
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
+const sharky = ["Sharky", "shark", 20, "Left Coast"];
+const plantee = ["Plantee", "plant", 5000, "Mordor"];
+const porgee = ["Porgee", "Porg", 186, "Ach-To"];
+const dart = ["D'Art", "Demogorgon Dog", 2, "Upside Down"];
+
+let newAge = 0;
+
+for(let i = 0; i < plantee.length; i++){
+    if(typeof plantee[i] == 'number'){
+        newAge = plantee[i];
+        newAge += 1;
+        plantee[i] = newAge;
     }
 }
+console.log(plantee);
+
+let newHomeTown = "Gotham City";
+
+for(let i = 0; i < wolfy.length; i++){
+    if(i == 3){
+        wolfy[i] = newHomeTown;
+    }
+}
+console.log(wolfy);
+
+let secondTown = "Hawkins";
+
+dart.push(secondTown);
+console.log(dart);
+
+let replaceName = "Gameboy";
+
+for(let i = 0; i < wolfy.length; i++){
+    if(wolfy[i] == "Wolfy"){
+        wolfy[i] = replaceName;
+    }
+}
+console.log(wolfy);
